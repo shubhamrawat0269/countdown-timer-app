2d82ba4543918a5017c9f9303ccc79072416159f
shubham Rawat <rawatshubam269@gmail.com>
2024-04-03 11:32:05 +0530

    Desktop design UI done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import CountdownTimer from "./components/CountdownTimer";
 
 function App() {
   return (
-    <div>
+    <div className="primary__bg w-screen h-screen flex justify-center items-center">
       <CountdownTimer />
     </div>
   );
--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -2,29 +2,40 @@ import React from "react";
 
 const CountdownTimer = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Countdown Timer</h1>
-      <form onSubmit={() => {}}>
+    <div className="flex justify-center gap-12 items-center flex-col secondary__bg p-10">
+      <h1 className="text-3xl font-bold">Countdown Timer</h1>
+      <form
+        onSubmit={() => {}}
+        className="flex flex-col gap-3 justify-center items-center"
+      >
         <label htmlFor="datetime">
-          <input type="datetime" name="datetime" id="datetime" />
+          <input
+            type="datetime-local"
+            className=" p-2 outline-none rounded-lg border__style"
+            name="datetime"
+            id="datetime"
+            autoCapitalize="off"
+          />
         </label>
 
-        <button>Start Timer</button>
-        <button>Cancel Timer</button>
+        <div className="flex gap-5">
+          <button className="btn">Start Timer</button>
+          <button className="btn">Cancel Timer</button>
+        </div>
       </form>
 
-      <div>
-        <div>
-          <span>0</span> Days
+      <div className="flex justify-center gap-8 items-center ">
+        <div className="flex justify-center gap-2 items-center flex-col timer__bg p-5 w-40">
+          <span className="font-bold text-7xl">0</span> Days
         </div>
-        <div>
-          <span>0</span> Hours
+        <div className="flex justify-center gap-2 items-center flex-col timer__bg p-5 w-40">
+          <span className="font-bold text-7xl">0</span> Hours
         </div>
-        <div>
-          <span>0</span> Minutes
+        <div className="flex justify-center gap-2 items-center flex-col timer__bg p-5 w-40">
+          <span className="font-bold text-7xl">0</span> Minutes
         </div>
-        <div>
-          <span>0</span> Seconds
+        <div className="flex justify-center gap-2 items-center flex-col timer__bg p-5 w-40">
+          <span className="font-bold text-7xl">0</span> Seconds
         </div>
       </div>
     </div>
