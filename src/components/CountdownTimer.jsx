1130fad3cad5c08ec24cfde276afadbea33d9fc9
shubham Rawat <rawatshubam269@gmail.com>
2024-04-04 12:07:30 +0530

    Timer Details shown in UI display done

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -3,22 +3,40 @@ import { TimerContext } from "../contexts/TimerContext";
 import { getTime } from "../utils";
 
 const CountdownTimer = () => {
-  const { isTimerStart, setTimerStart, inputTime, setInputTime } =
-    useContext(TimerContext);
+  const {
+    isTimerStart,
+    inputTime,
+    setInputTime,
+    handleTimer,
+    hours,
+    minutes,
+    seconds,
+    days,
+    setDays,
+    setSeconds,
+    setHours,
+    setMinutes,
+    error,
+  } = useContext(TimerContext);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     const userGivenTime = new Date(inputTime);
     const curr_time = new Date();
-    // console.log(userGivenTime);
-    // console.log(curr_time);
+
     const userGivenTimeINMilliSec = userGivenTime.getTime();
     const currTimeINMilliSec = curr_time.getTime();
     const reqTimeDiff = userGivenTimeINMilliSec - currTimeINMilliSec;
+
     if (reqTimeDiff > 0) {
       const { seconds, minutes, hours, days } = getTime(reqTimeDiff);
-      console.log({ seconds, minutes, hours, days });
+      setDays(days);
+      setSeconds(seconds);
+      setHours(hours);
+      setMinutes(minutes);
+
+      // handleTimer(seconds, minutes, hours, days);
     }
   };
 
@@ -46,7 +64,7 @@ const CountdownTimer = () => {
             <button
               type="submit"
               className="p-2.5 text-white bg-violet-900 hover:bg-violet-800"
-              onClick={() => setTimerStart(!isTimerStart)}
+              onClick={() => {}}
             >
               Start Timer
             </button>
@@ -54,26 +72,31 @@ const CountdownTimer = () => {
             <button
               type="submit"
               className="p-2.5 text-white bg-violet-900 hover:bg-violet-800"
-              onClick={() => setTimerStart(!isTimerStart)}
+              onClick={() => {}}
             >
               Cancel Timer
             </button>
           )}
         </div>
+        {error && (
+          <p className="text-red-800 font-bold text-center mt-2 mb-2">
+            {error}
+          </p>
+        )}
       </form>
 
       <div className="grid grid-cols-2 gap-2 lg:grid-cols-4 lg:gap-4">
         <div className="flex justify-center gap-2 items-center rounded-xl md:flex-col bg-violet-800 hover:bg-violet-900 text-white p-5 w-40">
-          <span className="font-bold text-4xl">0</span> Days
+          <span className="font-bold text-4xl">{days}</span> Days
         </div>
         <div className="flex justify-center gap-2 items-center  rounded-xl  md:flex-col bg-violet-800 hover:bg-violet-900 text-white p-5 w-40">
-          <span className="font-bold text-4xl">0</span> Hours
+          <span className="font-bold text-4xl">{hours}</span> Hours
         </div>
         <div className="flex justify-center gap-2 items-center rounded-xl  md:flex-col bg-violet-800 hover:bg-violet-900 text-white p-5 w-40">
-          <span className="font-bold text-4xl">0</span> Minutes
+          <span className="font-bold text-4xl">{minutes}</span> Minutes
         </div>
         <div className="flex justify-center gap-2 items-center rounded-xl  md:flex-col bg-violet-800 hover:bg-violet-900 text-white p-5 w-40">
-          <span className="font-bold text-4xl">0</span> Seconds
+          <span className="font-bold text-4xl">{seconds}</span> Seconds
         </div>
       </div>
     </div>
--- src/contexts/TimerContext.jsx
+++ src/contexts/TimerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useRef, useState } from "react";
 
 const TimerContext = createContext();
 
@@ -9,6 +9,51 @@ const TimerProvider = ({ children }) => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [days, setDays] = useState(0);
+  const [error, setError] = useState("");
+  const timerRef = useRef(null);
+
+  function resetTimeToZero() {
+    setDays(0);
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+  }
+
+  function stopTimer() {
+    clearInterval(timerRef.current);
+  }
+
+  const handleTimer = (seconds, minutes, hours, days) => {
+    // validation check
+    if (seconds <= 59 && minutes <= 59 && hours <= 23 && days <= 99) {
+      console.log({ seconds, minutes, hours, days });
+      timerRef.current = setInterval(() => {
+        timer();
+      }, 1000);
+    } else {
+      setError("Selected Time is more than 100 days");
+    }
+  };
+
+  function timer() {
+    if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
+      resetTimeToZero();
+      stopTimer();
+    } else if (seconds != 0) {
+      setSeconds((preVal) => preVal - 1);
+    } else if (minutes != 0 && seconds == 0) {
+      setSeconds("59");
+      setMinutes((preVal) => preVal - 1);
+    } else if (hours != 0 && minutes == 0) {
+      setMinutes("60");
+      setHours((preVal) => preVal - 1);
+    } else if (days != 0 && hours == 0) {
+      setHours("23");
+      setDays((preVal) => preVal - 1);
+    }
+    setTimerStart(!isTimerStart);
+  }
+
   const context = {
     inputTime,
     isTimerStart,
@@ -16,12 +61,14 @@ const TimerProvider = ({ children }) => {
     minutes,
     seconds,
     days,
+    error,
     setDays,
     setSeconds,
     setHours,
     setMinutes,
     setTimerStart,
     setInputTime,
+    handleTimer,
   };
 
   return (
